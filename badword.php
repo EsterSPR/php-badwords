a3f26c6b8d9978d39fc4af8b767c5abeb69fdacd
EsterSPR <web.esterspreafico@gmail.com>
2023-01-30 17:22:12 +0100

    completato esercizio

--- badword.php
+++ badword.php
@@ -1,5 +1,11 @@
 <?php
-    $bigtext = $_GET('bigtext'); 
-    $badword = $_GET('badword'); 
+    $bigtext = $_POST['bigtext']; 
+    $badword = $_POST['badword']; 
 
+    echo $bigtext."<br/><br/>";
+    echo "Il testo è lungo ".strlen($bigtext)." caratteri <br/><br/><hr/>";
+
+    $bigtext_censored = str_replace($badword, '***', $bigtext);
+    echo "<br/>".$bigtext_censored."<br/><br/>";
+    echo "Il testo <u>censurato</u> è lungo ".strlen($bigtext_censored)." caratteri";
 ?>
